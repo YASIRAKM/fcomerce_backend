0fa5bfa7e900555d7f2187711575ac0225b9557a
yasir <yasirakm11@gmail.com>
2024-12-03 17:15:00 +0530

    second

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.23.3
 
 require github.com/labstack/echo/v4 v4.12.0
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
--- router/router.go
+++ router/router.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORS())
 	// Define a simple route
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Echo!")
